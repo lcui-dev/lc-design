30de7d2480fc46a945736f8a1f5e3b8212e806b8
Liu Chao <lc-soft@live.cn>
2017-12-31 16:44:34 +0800

    Add scrollbar to modal

--- src/ui/components/modal.c
+++ src/ui/components/modal.c
@@ -1,9 +1,13 @@
 #include <LCUI_Build.h>
 #include <LCUI/LCUI.h>
+#include <LCUI/timer.h>
 #include "ui/components.h"
+#include <LCUI/gui/widget/scrollbar.h>
 
 typedef struct LCUIEx_ModalRec_ {
 	LCUI_Widget backdrop;
+	LCUI_Widget scrollbar;
+	LCUI_Widget dialog;
 } LCUIEx_ModalRec, *LCUIEx_Modal;
 
 static struct LCUIEx_ModalModule {
@@ -14,7 +18,12 @@ static void Modal_Init( LCUI_Widget w )
 {
 	const size_t size = sizeof( LCUIEx_ModalRec );
 	LCUIEx_Modal modal = Widget_AddData( w, self.proto, size );
+
+	modal->dialog = NULL;
 	modal->backdrop = NULL;
+	modal->scrollbar = LCUIWidget_New( "scrollbar" );
+	Widget_Append( w, modal->scrollbar );
+	ScrollBar_BindBox( modal->scrollbar, w );
 }
 
 static void Modal_Destroy( LCUI_Widget w )
@@ -23,6 +32,21 @@ static void Modal_Destroy( LCUI_Widget w )
 	if( modal->backdrop ) {
 		Widget_Destroy( modal->backdrop );
 		modal->backdrop = NULL;
+	}
+}
+
+static void Modal_InitDialog( LCUI_Widget w )
+{
+	LinkedListNode *node;
+	LCUIEx_Modal modal = Widget_GetData( w, self.proto );
+	for( LinkedList_Each( node, &w->children ) ) {
+		if( Widget_HasClass( node->data, "modal-dialog" ) ) {
+			modal->dialog = node->data;
+			break;
+		}
+	}
+	if( modal->dialog ) {
+		ScrollBar_BindLayer( modal->scrollbar, modal->dialog );
 	}
 }
 
@@ -35,8 +59,11 @@ void Modal_Show( LCUI_Widget w )
 		Widget_AddClass( modal->backdrop, "modal-backdrop" );
 		Widget_Append( root, modal->backdrop );
 	}
+	if( !modal->dialog ) {
+		Modal_InitDialog( w );
+	}
 	Widget_Append( root, w );
-	Widget_Show( w );
+	Widget_AddClass( w, "show" );
 }
 
 void Modal_Hide( LCUI_Widget w )
@@ -46,7 +73,7 @@ void Modal_Hide( LCUI_Widget w )
 		Widget_Destroy( modal->backdrop );
 		modal->backdrop = NULL;
 	}
-	Widget_Hide( w );
+	Widget_RemoveClass( w, "show" );
 }
 
 void LCUIEx_InitModal( void )
