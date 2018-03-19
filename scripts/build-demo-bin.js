ab10cc9e164a65057cfb1e845a57650df91f794c
Liu Chao <lc-soft@live.cn>
2018-03-19 20:10:46 +0800

    Update scripts/build-demo-bin.js

--- scripts/build-demo-bin.js
+++ scripts/build-demo-bin.js
@@ -4,9 +4,8 @@ var child_process = require('child_process')
 if (process.platform == 'win32' && msbuild.exists()) {
   msbuild.exec('.\\scripts\\build-demo-bin')
 } else {
-  child_process.exec('xmake --project=demo', function (err, stdout, stderr) {
+  child_process.exec('xmake build demo', function (err, stdout, stderr) {
     console.log(stdout)
     console.log(stderr)
   })
 }
-
