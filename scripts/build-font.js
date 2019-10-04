e4401e6006304cded8074de3237dd7946bc30553
Liu <lc-soft@live.cn>
2019-10-04 22:30:23 +0800

    build: update output path

--- scripts/build-font.js
+++ scripts/build-font.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
-var fontdir = 'dist/assets/fonts/';
+const fs = require('fs')
+const fontdir = 'dist/app/assets/fonts/'
 
 if (!fs.existsSync(fontdir)) {
-  fs.mkdirSync(fontdir);
+  fs.mkdirSync(fontdir)
 }
 
 fs.createReadStream('./node_modules/@mdi/font/fonts/materialdesignicons-webfont.ttf')
-  .pipe(fs.createWriteStream(fontdir + 'iconfont.ttf'));
+  .pipe(fs.createWriteStream(fontdir + 'iconfont.ttf'))
